67fecc6b07fa2358dda20a9e365b76ac68fa96f2
Grote <buildtag@DESKTOP-EUATDIC>
2018-10-09 15:59:19 -0700

    Bugfix: PRTGResult Tests for older pester format

--- Tests/Add-PRTGResult.Tests.ps1
+++ Tests/Add-PRTGResult.Tests.ps1
@@ -7,7 +7,7 @@ import-module (join-path $ModulePath '*.psd1') -force
 Describe 'Add-PRTGResultSet' {
     It 'Add-PRTGResult -Channel -Value' {
         $testResultSet = Add-PRTGResult -Channel "Test" -Value 5
-        $testResultSet['prtg'].result[0].channel | Should -Be 'Test'
-        $testResultSet['prtg'].result[0].value | Should -Be 5
+        $testResultSet['prtg'].result[0].channel | Should Be 'Test'
+        $testResultSet['prtg'].result[0].value | Should Be 5
     }
 }
